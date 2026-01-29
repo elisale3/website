1cb3d13617dd745dfb460cd4078a33801ea2f500
trsakamoto <trsakamoto@ucsd.edu>
2026-01-28 16:34:38 -0800

    Fix ESLint map return errors in TeamGrid

--- src/components/teamComponents/teamGrid.js
+++ src/components/teamComponents/teamGrid.js
@@ -1,4 +1,4 @@
-import React from "react";
+{/*import React from "react";
 import { Container, Row, Col } from "react-grid-system";
 import {teamInfo} from "./teamInfo"
 import Polaroid from "./polaroid";
@@ -93,6 +93,7 @@ class TeamGrid extends React.Component {
                       }
                   })}
               </Row>
+              
 
               <Row className="top-margin-section">
                 <h4>Photography</h4>
@@ -251,6 +252,77 @@ class TeamGrid extends React.Component {
         </Container>
       </div>
     )
+  }
+}
+
+export default TeamGrid;
+*/}
+import React from "react";
+import { Container, Row, Col } from "react-grid-system";
+import { teamInfo } from "./teamInfo";
+import Polaroid from "./polaroid";
+import "../../styles/TeamGrid.scss";
+
+class TeamGrid extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { showModal: -1 };
+  }
+
+  openModal = (value) => {
+    this.setState({ showModal: value });
+  };
+
+  hideModal = () => {
+    this.setState({ showModal: -1 });
+  };
+
+  renderSection = (title, position) => (
+    <>
+      <Row className="top-margin-section">
+        <h4>{title}</h4>
+      </Row>
+      <Row>
+        {teamInfo
+          .filter((member) => member.position === position)
+          .map((teamMember, idx) => (
+            <Col xs={6} sm={6} md={3} l={4} key={teamMember.name}>
+              <div onClick={() => this.openModal(idx)}>
+                <Polaroid
+                  picture={teamMember.picture}
+                  name={teamMember.name}
+                />
+              </div>
+            </Col>
+          ))}
+      </Row>
+    </>
+  );
+
+  render() {
+    return (
+      <div className="team-grid-section">
+        <Container className="team-grid-container">
+          <Row className="top-margin-section">
+            <h2>TREND Team</h2>
+          </Row>
+          <Row className="top-margin-subsection">
+            <h3>2024 - 2025</h3>
+          </Row>
+
+          <div className="team">
+            {this.renderSection("Editors in Chief", "Editor in Chief")}
+            {this.renderSection("Photography", "Photographer")}
+            {this.renderSection("Writing", "Writer")}
+            {this.renderSection("Graphic Design", "Graphic Designer")}
+            {this.renderSection("External", "External")}
+            {this.renderSection("Marketing", "Marketing")}
+            {this.renderSection("Styling", "Stylist")}
+            {this.renderSection("Web Development", "Web Developer")}
+          </div>
+        </Container>
+      </div>
+    );
   }
 }
 
